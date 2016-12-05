52474931b9d436c7e589c373be5b1dcdac02e09d
Pacoogle89 <p.ragozzino@paymove.eu>
2016-12-05 12:49:08 +0100

    3° commit di test da phpstorm

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
     <h3>Il portale &egrave; momentaneamente in manutenzione.</h3>
     <h3>Saremo online dalle 12:00.</h3>
 </div>
-    <div> prova antonio 3</div>
+    <div> prova antonio 5</div>
     <div>Che cumbinat?</div>
 </body>
