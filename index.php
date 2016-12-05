54230a7d1342232c2d2e7d9c956451119b1c76a3
Pacoogle89 <p.ragozzino@paymove.eu>
2016-12-05 12:33:09 +0100

    Inserito primo file di test per provare github

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p.ragozzino
+ * Date: 03/06/2016
+ * Time: 11:08
+ */
+
+?>
+
+<body>
+<div style="text-align: center">
+    <h1><img src="img/logo.png"></h1>
+    <h3>Il portale &egrave; momentaneamente in manutenzione.</h3>
+    <h3>Saremo online dalle 12:00.</h3>
+</div>
+</body>
